020004712b44ab67294f2057a4bd1b6b14bdd74e
daniil <daniil_baldin@ukr.net>
2023-02-03 17:19:32 +0200

    First commit(Added main layout, basic routes, started login page)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { Layout } from './Components/Layout/Layout';
+import { Login } from './Pages/Login';
 
 function App() {
     return (
-        <div>
-            <p>Hello there, General Kenobi.</p>
-        </div>
+        <Routes>
+            <Route
+                path='/'
+                element={<Layout />}
+            >
+                <Route
+                    path='/'
+                    element={<Login />}
+                />
+            </Route>
+        </Routes>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import './Assets/main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </BrowserRouter>
+);
